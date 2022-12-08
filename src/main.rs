e8baaaf22cbef7607742771e98e8b3df2b2c7b58
jordan araujo <jordasg@gmail.com>
2022-12-08 13:03:42 -0300

    initial: simplest simula_camera I could build

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*, render::view::RenderLayers};
+use simula_camera::{flycam::*, orbitcam::*};
+use simula_action::ActionPlugin;
+use simula_video::rt;
+use bevy_egui::EguiPlugin;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
+fn main() {
+    let mut app = App::new();
+
+    app.insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WIDTH,
+                height: HEIGHT,
+                title: "Minigame2".to_string(),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugin(EguiPlugin)
+        .add_plugin(ActionPlugin)
+        .add_plugin(OrbitCameraPlugin)
+        .add_plugin(FlyCameraPlugin)
+        .add_startup_system(spawn_scene)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn spawn_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+            material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+            ..Default::default()
+        })
+        .insert(Name::new("Floor"));
+}
+
+fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    //   commands.spawn(Camera3dBundle {
+    //     transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    //     ..Default::default()
+    // });
+    let rt_image = images.add(rt::common_render_target_image(UVec2 { x: 256, y: 256 }));
+
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 2.0, -10.0)
+                .looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
+            ..default()
+        })
+        .insert(RenderLayers::all())
+        .with_children(|parent| {
+            let mut _child = parent.spawn(Camera3dBundle {
+                camera_3d: Camera3d {
+                    clear_color: ClearColorConfig::Custom(Color::BLACK),
+                    ..default()
+                },
+                camera: Camera {
+                    priority: -1,
+                    target: bevy::render::camera::RenderTarget::Image(rt_image.clone()),
+                    ..default()
+                },
+                ..default()
+            });
+
+            #[cfg(feature = "gst")]
+            _child.insert(GstSrc {
+                size: UVec2 { x: 256, y: 256 },
+                ..default()
+            });
+        })
+        .insert(FlyCamera::default());
+}
